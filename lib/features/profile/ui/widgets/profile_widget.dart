c7c65d55b2ed1f72f8f1384bcd3f1f056538b864
jayashz <jayzstha15@gmail.com>
2025-05-11 13:24:11 +0545

    Added: profile page

--- lib/features/profile/ui/widgets/profile_widget.dart
+++ lib/features/profile/ui/widgets/profile_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:kabar_news/common/bloc/assets.dart';
+import 'package:kabar_news/common/router/route_path.dart';
 
 class ProfileWidget extends StatelessWidget {
   const ProfileWidget({super.key});
@@ -6,8 +9,65 @@ class ProfileWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Profile screen"),
+      appBar: AppBar(
+        title: Text('Profile'),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).primaryColor,
+        iconTheme: Theme.of(context).appBarTheme.iconTheme,
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.push(RoutePath.settings);
+            },
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 60,
+              backgroundImage: AssetImage(Assets.user),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              "John doe",
+              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "The typical user of this news app is a curious and informed individual who values quick access to reliable and diverse news content. Whether commuting, working, or relaxing, the user expects smooth navigation, quick loading, and fresh updates, often revisiting the app multiple times a day to stay current.",
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    color: Colors.grey[700],
+                  ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: Theme.of(context).elevatedButtonTheme.style,
+                  onPressed: () {},
+                  child: Text("Share profile"),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Edit profile"),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
